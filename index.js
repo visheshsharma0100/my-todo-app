4e9e2c7ed9520bdb044969b324d1511faa6c4f4b
Vishesh Sharma <visheshsharm0100@gmail.com>
2026-03-16 03:32:01 +0530

    fix cors order

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const jwt=require('jsonwebtoken');
 const cors = require('cors');
-app.use(cors());
 const app=express();
+app.use(cors());
+
 
 app.use(express.json());
 app.use(express.static(__dirname + "/Frontend"));
